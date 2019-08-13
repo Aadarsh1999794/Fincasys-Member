a343fda11100dec1e7a3808900f108b5765d314e
harsh panchal <harshpanchal.silverwing@gmail.com>
2019-08-13 10:06:26 +0530

    modified file

--- Finca/Application Layer/Controller/profile/profile member/FamilyMembersVC.swift
+++ Finca/Application Layer/Controller/profile/profile member/FamilyMembersVC.swift
@@ -20,14 +20,5 @@ class FamilyMembersVC: UIViewController {
     @IBAction func btnBackPressed(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    helloooooo
 }
